2ba9140748db1746e0da52451e00e6c147a312fb
Dmitry Shevchenkoo <dshevchenkoo@gmail.com>
2018-08-17 02:41:45 +0600

    Отобразил массив статей.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Article from './Article'
+import ArticleList from './ArticleList'
 import articles from '../fixtures'
 
 
@@ -7,7 +7,7 @@ function App() {
 	return(
 		<div>
 			 <h1>Hello world!</h1>
-			 <Article article={articles[0]}/>
+			 <ArticleList articles={articles}/>
 		</div>
 	)
 }
